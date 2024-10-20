c72c1f9b1448d3f0a5291f4734b45dab4d8eaf02
Saffanah aljawi <Sufanaaljawi@gmail.com>
2024-10-20 10:57:30 +0300

    Initial Commit

---
+++ PlantsReminder/PlantsReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsReminderApp.swift
+//  PlantsReminder
+//
+//  Created by Saffanah Aljawi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantsReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantsReminderTests/PlantsReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsReminderTests.swift
+//  PlantsReminderTests
+//
+//  Created by Saffanah Aljawi on 17/04/1446 AH.
+//
+
+import Testing
+@testable import PlantsReminder
+
+struct PlantsReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
